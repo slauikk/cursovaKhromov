e18470e4f23677554e1da21bf231746293534fc7
slauikk <zzzyaroslav63@gmail.com>
2024-11-23 12:30:32 +0200

    fix: apiservice

--- currentapiservice.cpp
+++ currentapiservice.cpp
@@ -11,26 +11,26 @@ void CurrentAPIService::fetchExchangeRates()
     QNetworkRequest request(url);
     QNetworkReply *reply = manager->get(request);
 
-
     connect(reply, &QNetworkReply::finished, this, &CurrentAPIService::onReplyFinished);
 }
 
 void CurrentAPIService::onReplyFinished()
 {
-
     QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
     if (!reply)
         return;
 
-    if (reply->error() == QNetworkReply::NoError) {
+    QString usdBuy = "40.50";
+    QString usdSell = "41.00";
+    QString eurBuy = "42.50";
+    QString eurSell = "43.00";
 
+    if (reply->error() == QNetworkReply::NoError) {
         QByteArray responseData = reply->readAll();
         QJsonDocument jsonDoc = QJsonDocument::fromJson(responseData);
 
         if (jsonDoc.isArray()) {
             QJsonArray ratesArray = jsonDoc.array();
-            QString usdBuy, usdSell, eurBuy, eurSell;
-
 
             for (const QJsonValue &value : ratesArray) {
                 QJsonObject obj = value.toObject();
@@ -47,16 +47,18 @@ void CurrentAPIService::onReplyFinished()
                     }
                 }
             }
-
-            qDebug() << "USD: Buy =" << usdBuy << ", Sell =" << usdSell;
-            qDebug() << "EUR: Buy =" << eurBuy << ", Sell =" << eurSell;
-
-
-            emit exchangeRatesFetched(usdBuy, usdSell, eurBuy, eurSell);
+        } else {
+            qDebug() << "Response is not a valid JSON array.";
         }
     } else {
         qDebug() << "Error fetching exchange rates:" << reply->errorString();
+        qDebug() << "Using default exchange rates.";
     }
+
+    qDebug() << "USD: Buy =" << usdBuy << ", Sell =" << usdSell;
+    qDebug() << "EUR: Buy =" << eurBuy << ", Sell =" << eurSell;
+
+    emit exchangeRatesFetched(usdBuy, usdSell, eurBuy, eurSell);
 
     reply->deleteLater();
 }
